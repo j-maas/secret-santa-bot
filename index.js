07743c64cc663e598bada5c087cb08d40f25dc9a
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-11-22 18:57:09 +0100

    Implement /start with callback keyboard button

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const Telegraf = require('telegraf')
+const Markup = require('telegraf/markup')
+
+const util = require('util')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
+async function log_middleware (ctx, next) {
+  console.log('===================================================')
+  console.log('Incoming:\n', util.inspect(ctx.update, {depth: 5}))
+  console.log('- - - - - - - - - - - - - - - - - - - - - - - - - -')
+  console.log('Outgoing:\n', await next())
+  console.log('===================================================')
+}
+
+bot.use(log_middleware)
+
+async function catch_error (ctx, next) {
+  let result = null
+  try {
+    result = await next()
+  } catch (e) {
+    console.error(e)
+  }
+  return result
+}
+
+bot.use(catch_error)
+
 bot.start((ctx) => {
-  console.log('started:', ctx.from.id)
-  return ctx.reply('Welcome!')
+  return ctx.reply(
+    'Your secret santa circle is opened.',
+    Markup.inlineKeyboard(
+      [Markup.callbackButton('Join', 'join')]
+    ).extra()
+  )
 })
-bot.command('help', (ctx) => ctx.reply('Try send a sticker!'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
-bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
+
+bot.action('join', async (ctx) => {
+  return ctx.answerCbQuery('You will be added. Maybe.')
+})
 
 bot.startPolling()
