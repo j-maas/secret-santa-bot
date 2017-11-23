ec0090f3f66821d95692d4e1ab64e5a77d5c3bb0
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-11-23 10:54:42 +0100

    Factor out message editing

--- secretSantaHandler.js
+++ secretSantaHandler.js
@@ -11,8 +11,8 @@ class SecretSantaHandler {
   registerToBot (bot) {
     bot.use(this.log_middleware)
     bot.use(this.catch_error)
-    bot.start(async (ctx) => {if (ctx.chat.type === 'private') return this.listChildren(ctx)})
-    bot.command('child', this.listChildren)
+    bot.start(async (ctx) => {if (ctx.chat.type === 'private') return this.listChild(ctx)})
+    bot.command('child', this.listChild)
     bot.on('inline_query', ({inlineQuery, answerInlineQuery}) => this.inlineQuery(inlineQuery, answerInlineQuery))
     bot.on('chosen_inline_result', ({chosenInlineResult}) => this.chosenInlineResult(chosenInlineResult))
     bot.action('join', (ctx) => this.join(ctx))
@@ -37,7 +37,7 @@ class SecretSantaHandler {
     return result
   }
 
-  async listChildren (ctx) {
+  async listChild (ctx) {
     const currentUserId = ctx.from.id
     if (Object.keys(this.matches).includes(currentUserId.toString())) {
       return ctx.reply(`Your child is ${this.matches[currentUserId].first_name}.`)
@@ -46,18 +46,15 @@ class SecretSantaHandler {
 
   async inlineQuery (inlineQuery, answerInlineQuery) {
     const offset = parseInt(inlineQuery.offset) || 0
+    const message = this.getStatusMessage(false)
     const responses = [{
       type: 'article',
       id: 1,
       title: 'Secret Santa Group',
       input_message_content: {
-        message_text: 'Your secret santa circle is opened.',
+        message_text: message.messageText,
       },
-      reply_markup: Markup.inlineKeyboard(
-        [
-          Markup.callbackButton('Join', 'join'),
-        ]
-      )
+      reply_markup: message.markup
     }]
 
     const results = responses.splice(offset)
@@ -74,12 +71,24 @@ class SecretSantaHandler {
     if (same_user_in_list.length === 0) {
       this.users.push(user)
     }
-    const messageText = 'The secret santa circle has the following members:\n\n' + this.users.map(user => '\t - ' + user.first_name).join('\n')
-    await ctx.telegram.callApi('editMessageText', Object.assign({
-      inline_message_id: ctx.callbackQuery.inline_message_id,
-      text: messageText,
-    }, Markup.inlineKeyboard([Markup.callbackButton('Join', 'join')]).extra()))
+    await this.updateStatusMessage(ctx, ctx.callbackQuery.inline_message_id)
     return ctx.answerCbQuery()
+  }
+
+  async updateStatusMessage (ctx, inline_message_id, isClosed) {
+    const message = this.getStatusMessage(isClosed)
+    await ctx.telegram.callApi('editMessageText', Object.assign({
+      inline_message_id: inline_message_id,
+      text: message.messageText,
+    }, message.markup.extra()))
+  }
+
+  getStatusMessage (isClosed) {
+    return {
+      messageText: 'The secret santa circle has the following members:\n\n' + this.users.map(user => '\t - ' + user.first_name).join('\n'),
+      markup: isClosed ? Markup.inlineKeyboard([Markup.urlButton('See your child', 'https://telegram.me/Y0hy0hTestBot?start=child')])
+        : Markup.inlineKeyboard([Markup.callbackButton('Join', 'join')]),
+    }
   }
 
   shuffle (array) {
@@ -107,11 +116,8 @@ class SecretSantaHandler {
   async close (ctx) {
     this.matches = this.match(Array.from(this.users))
 
-    this.messages.forEach(inline_message_id => ctx.telegram.callApi('editMessageText', Object.assign({
-      inline_message_id: inline_message_id,
-      text: this.users.map(user => user.first_name),
-    }, Markup.inlineKeyboard([Markup.urlButton('See your child', 'https://telegram.me/Y0hy0hTestBot?start=child')]).extra())))
+    this.messages.forEach(inline_message_id => this.updateStatusMessage(ctx, inline_message_id, true))
   }
 }
 
-module.exports = { SecretSantaHandler }
+module.exports = {SecretSantaHandler}
