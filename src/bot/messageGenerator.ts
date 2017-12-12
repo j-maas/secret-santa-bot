1f07e399a20e449ecb785c9e325b61ea6a98b917
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-12-12 19:56:34 +0100

    Apply standard code style to code base

--- src/bot/messageGenerator.ts
+++ src/bot/messageGenerator.ts
@@ -3,52 +3,52 @@ import { User } from 'telegraf/typings/telegram-types'
 const Markup = require('telegraf/markup')
 
 export class MessageGenerator {
-    static getStatusMessage(users: Array<User>, isClosed: boolean, botName: string): MessageTemplate {
-        if (isClosed) {
-            return new MessageTemplate(
-                `Done! The secret santa circle is now closed`
-                + ` and has the following ${users.length} members:\n`
-                + MessageGenerator.getUsersList(users),
+  static getStatusMessage (users: Array<User>, isClosed: boolean, botName: string): MessageTemplate {
+    if (isClosed) {
+      return new MessageTemplate(
+                `Done! The secret santa circle is now closed` +
+                ` and has the following ${users.length} members:\n` +
+                MessageGenerator.getUsersList(users),
                 Markup.inlineKeyboard([
-                                          Markup.urlButton(
+                  Markup.urlButton(
                                               'See your child',
-                                              `https://telegram.me/${botName}?start=child`,
-                                          ),
-                                      ]),
+                                              `https://telegram.me/${botName}?start=child`
+                                          )
+                ])
             )
-        } else {
-            return new MessageTemplate(
-                `The secret santa circle has the following ${users.length} members:\n`
-                + MessageGenerator.getUsersList(users),
-                Markup.inlineKeyboard([Markup.callbackButton('Join', 'join')]),
+    } else {
+      return new MessageTemplate(
+                `The secret santa circle has the following ${users.length} members:\n` +
+                MessageGenerator.getUsersList(users),
+                Markup.inlineKeyboard([Markup.callbackButton('Join', 'join')])
             )
-        }
     }
+  }
 
-    static getUsersList(users: Array<User>): string {
-        return users.map(user => `\t - ${MessageGenerator.getUserName(user)}`).join('\n')
-    }
+  static getUsersList (users: Array<User>): string {
+    return users.map(user => `\t - ${MessageGenerator.getUserName(user)}`).join('\n')
+  }
 
-    static getUserName(user: User): string {
-        let output = `${user.first_name}`
-        if (user.last_name) {
-            output += ` ${user.last_name}`
-        }
-        if (user.username) {
-            const maxLength = 16
-            if (user.username.length > maxLength) {
-                output += ` (${user.username.substring(0, maxLength)}…)`
-            } else {
-                output += ` (${user.username.substring(0, maxLength)})`
-            }
-        }
-        return output
+  static getUserName (user: User): string {
+    let output = `${user.first_name}`
+    if (user.last_name) {
+      output += ` ${user.last_name}`
     }
+    if (user.username) {
+      const maxLength = 16
+      if (user.username.length > maxLength) {
+        output += ` (${user.username.substring(0, maxLength)}…)`
+      } else {
+        output += ` (${user.username.substring(0, maxLength)})`
+      }
+    }
+    return output
+  }
 }
 
 export class MessageTemplate {
-    constructor(
+  constructor (
         public messageText: string,
-        public inlineKeyboard,
+        public inlineKeyboard
     ) {}
 }
--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,15 @@ const proxy = require('express-http-proxy')
 
 startServer(express())
 
-async function startServer(app: Express) {
-    // Static website
-    app.use(express.static('./public'))
+async function startServer (app: Express) {
+  // Static website
+  app.use(express.static('./public'))
 
-    // Bot webhooks redirect
-    const {pathToFetchFrom, urlToRedirectTo} = await startBot()
-    app.use(pathToFetchFrom, proxy(urlToRedirectTo))
+  // Bot webhooks redirect
+  const {pathToFetchFrom, urlToRedirectTo} = await startBot()
+  app.use(pathToFetchFrom, proxy(urlToRedirectTo))
 
-    const listener = app.listen(process.env.PORT, function () {
-        console.log('Your app is listening on port ' + listener.address().port)
-    })
+  const listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + listener.address().port)
+  })
 }
